b9452f9db9188056a9d1e0a9ba00f68b9a25abd6
ARAVIND1702 <20cs013@kpriet.ac.in>
2023-03-30 23:02:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-l h-screen relative from-[#000d09b9] to-[#005c3f65] ">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
